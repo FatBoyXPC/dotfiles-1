a6f899efbb59f3d16966085f2f4e29d236fd2515
James LaChance <fatboyxpc@gmail.com>
2019-05-02 04:11:39 -0400

    Pairing w/@jfly to add EDID switching for autoperipherals

--- bin/playing-with-arandr-screenlayout.py
+++ bin/playing-with-arandr-screenlayout.py
@@ -2,6 +2,7 @@
 
 from pprint import pprint
 import screenlayout.xrandr
+from edid import Edid
 from screenlayout.auxiliary import Position, NORMAL, InadequateConfiguration
 
 xrandr = None
@@ -11,14 +12,20 @@ def main():
     xrandr.load_from_x()
 
     outputs_by_name = xrandr.state.outputs
-    connected_outputs = [output for output in outputs_by_name.values() if output.connected]
     for output in outputs_by_name.values():
-        # Disable the laptop screen if there's another output connected.
-        if output.name == 'eDP1' and len(connected_outputs) > 1:
-            make_active = False
-        else:
-            make_active = output.connected
-        activate_output(output, make_active=make_active)
+        activate_output(output, make_active=False)
+
+    def find_output(monitor_name):
+        output_name = find_monitor(monitor_name)
+        return outputs_by_name[output_name] if output_name else None
+
+    benq_output = find_output('BenQ XL2420T')
+    laptop_output = outputs_by_name['eDP1']
+    if benq_output:
+        activate_output(benq_output, make_active=True)
+    else:
+        activate_output(laptop_output, make_active=True)
+
 
     print(" ".join(xrandr.configuration.commandlineargs()))
 
@@ -40,9 +47,37 @@ def activate_output(output, make_active):
             else:
                 raise InadequateConfiguration(
                     "Smallest mode too large for virtual.")
-            output_config.mode = first_mode
 
-            output_config.rotation = NORMAL
+        output_config.mode = first_mode
+        output_config.rotation = NORMAL
+
+def get_edids():
+    output = xrandr._output("--verbose")
+    display = None
+    edid = None
+    edids_by_display = {}
+
+    for line in output.split('\n'):
+        if 'connected' in line:
+            display = line.split(' ')[0]
+        elif line.startswith('\tEDID'):
+            edid = ""
+        elif line.startswith('\t\t') and edid is not None:
+            edid += line.strip()
+        elif not line.startswith('\t\t') and edid is not None:
+            edid = bytes([ int("".join(pair), 16) for pair in zip(*[iter(edid)]*2) ])
+            edids_by_display[display] = Edid(edid)
+            edid = None
+            display = None
+
+    return edids_by_display
+
+def find_monitor(monitor_name):
+    for display_name, edid in get_edids().items():
+        if edid.name == monitor_name:
+            return display_name
+
+    return None
 
 if __name__ == "__main__":
     main()
